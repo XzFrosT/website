ff9c28b96af90e6c470581904489f24eaeb3ee82
XzFrosT <mschodeihsan@gmail.com>
2023-01-28 08:13:21 +0700

    added skills

--- data/Skills.js
+++ data/Skills.js
@@ -1,4 +1,11 @@
-import { SiCplusplus, SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTypescript } from 'react-icons/si';
+import { 
+	SiCmake, SiCplusplus, SiCss3, 
+	SiHtml5,
+	SiJavascript, 
+	SiMongodb,
+	SiNextdotjs, SiNodedotjs, 
+	SiReact, 
+	SiTailwindcss, SiTypescript } from 'react-icons/si';
 
 export const Skills = [
 	{
@@ -17,30 +24,65 @@ export const Skills = [
 	},
 	{
 		id: 2,
+		name: "HTML5",
+		icon: (className) => {
+			return <SiHtml5 className={className}/>
+		}
+	},
+	{
+		id: 3,
+		name: "CSS3",
+		icon: (className) => {
+			return <SiCss3 className={className}/>
+		}
+	},
+	{
+		id: 10,
+		name: "MongoDB",
+		icon: (className) => {
+			return <SiMongodb className={className}/>
+		}
+	},
+	{
+		id: 4,
 		name: 'TypeScript',
 		icon: (className) => {
 			return <SiTypescript className={className}/>
 		}
 	},
 	{
-		id: 3,
+		id: 5,
 		name: "C++",
 		icon: (className) => {
     	return <SiCplusplus className={className}/>
     }
   },
   {
-		id: 4,
+		id: 6,
+		name: "CMake",
+		icon: (className) => {
+			return <SiCmake className={className}/>
+		}
+	},
+  {
+		id: 7,
 		name: 'React',
 		icon: (className) => {
 			return <SiReact className={className}/>
 		}
 	},
 	{
-		id: 5,
+		id: 8,
 		name: 'Next.js',
 		icon: (className) => {
 			return <SiNextdotjs className={className}/>
+		}
+	},
+	{
+		id: 9,
+		name: "Tailwind CSS",
+		icon: (className) => {
+			return <SiTailwindcss className={className}/>
 		}
 	}
 ];
