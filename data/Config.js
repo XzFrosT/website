a6ee6bccc22f7d62334d6b205d358d25be56c7a2
XzFrosT <mschodeihsan@gmail.com>
2023-12-25 13:15:32 +0700

    removed statically cdn
    use default domain instead, the service is buggy:/

--- data/Config.js
+++ data/Config.js
@@ -1,12 +1,11 @@
 export const Config = {
-	cdn: "cdn.statically.io",
-	domains: ["ihsan.fun"],
+	domains: ["www.ihsan.fun"],
 	name: "ihsan's portfolio",
 	description: "Let me introduce myself to you my name is ihsan, and this is my personal website. Welcome!"
 }
 
 export const staticImageLoader = ({ src, width, height }) => {
-	let path = "https://" + Config["cdn"] + "/img/" + Config["domains"][0] + "/f=auto/images" + src;
+	let path = "https://" + Config["domains"][0] + "/images" + src;
 	
 	return path;
 }
